ae6bc37b6944572f9e4eace257ca84214e9e26ad
TellurMan <87385428+TellurMan@users.noreply.github.com>
2021-07-20 22:52:35 +0300

    2 задание

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	yurlov "elma/task1"
+	//yurlov "elma/task1"
+	yurlov "elma/task2"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(yurlov.SolutionSquareGenerator(4, 3))
-	fmt.Println(yurlov.SolutionBinaryGap(-9_223_363_237_502_777_052))
+	/*
+		fmt.Println(yurlov.SolutionSquareGenerator(4, 3))
+		fmt.Println(yurlov.SolutionBinaryGap(-9_223_363_237_502_777_052))
+	*/
+
+	arr := []int{1, 3, 4, 5, 8, 9}
+	arr2 := yurlov.ArrayRotator(arr, -9)
+	arr2 = yurlov.ArrayRotator(arr2, 8)
+	arr2 = yurlov.ArrayRotator(arr2, 0)
+	arr2 = yurlov.ArrayRotator(arr2, 2)
+	arr[0] = 100
+	fmt.Println(arr)
+	fmt.Println(arr2)
+
+	arr3 := []int{3, 8, 9, 7, 3, 5, 8, 9, 6}
+	res := yurlov.GetUniqCount(arr3)
+	fmt.Println(res)
+
 }
---
+++ task2/day2_ArrayRotator.go
@@ -0,0 +1,14 @@
+package yurlov
+
+// циклично сдвигает срез на count элементов
+func ArrayRotator(array []int, count int) []int {
+	delim := (len(array) - count%len(array)) % len(array) // индекс-разделитель делит срез на 2 части, которые меняются местами
+
+	if delim == 0 {
+		tmp := make([]int, len(array)) // копирование и возврат текущего среза, так как в случае delim = 0
+		copy(tmp, array)               // в append новый срез не будет создан -> возврат ссылки на старый срез (а это плохо)
+		return tmp
+	}
+
+	return append(array[delim:], array[:delim]...)
+}
